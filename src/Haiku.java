42de36fa8e87c5010c09519e784ae8dfeab9c26a
30184195 <30184195@nescol.ac.uk>
2023-08-30 15:48:52 +0100

    .

---
+++ src/Haiku.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ian Simpson's link,");
+        System.out.println("Nescol's wisdom he imparts,");
+        System.out.println("Professional growth.");
+    }
+}
+
+
