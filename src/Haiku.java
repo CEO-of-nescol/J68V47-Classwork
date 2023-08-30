a5573e8fd3348acaef261e20d5a04fc96b3f3bd8
30184195 <30184195@nescol.ac.uk>
2023-08-30 15:49:46 +0100

    rggs

--- src/Haiku.java
+++ src/Haiku.java
@@ -1,11 +1,9 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Haiku {
     public static void main(String[] args) {
         System.out.println("Ian Simpson's link,");
         System.out.println("Nescol's wisdom he imparts,");
         System.out.println("Professional growth.");
     }
 }
-
-
