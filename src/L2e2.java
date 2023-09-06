ee03f600e4b6774da4dd092ba4767cabf2ddb8d2
30184195 <30184195@nescol.ac.uk>
2023-09-06 15:21:23 +0100

    number scrumbler, using all math operators

---
+++ src/L2e2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class L2e2 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Pick a number NOW");
+        int num = input.nextInt();
+        num = num*2;
+        num = num/4;
+        num = num-1;
+        num = num%2;
+        num = num +12;
+        System.out.println(num);
+
+
+
+
+    }
+}
