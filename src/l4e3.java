751a9aa501357df6a9013dafe305e1f07acd9c02
1982 Yugo GVL <143613172+CEO-of-nescol@users.noreply.github.com>
2023-09-27 11:23:58 +0100

    Update l4e3.java
    I did this in the silliest way i could, will try to delete redundant files outside of src l4e2 and l4e3

--- src/l4e3.java
+++ src/l4e3.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class l4e3 {
 
     public static void main(String[] args) {
@@ -8,11 +9,19 @@ public class l4e3 {
         System.out.println("What is the cost of your order? ");
         float cost = input.nextFloat();
 
-        if(cost < 50)
-            System.out.println("Your shipping cost is £10.00");
-        else
-            System.out.println("Your shipping cost is £0.00");
+        float shipCost = 0;
 
+        if(cost < 50) {
+            shipCost = 10;
+            System.out.println("Your shipping cost is £" + shipCost);
+        } else {
+            shipCost = 0;
+            System.out.println("Your shipping cost is £" + shipCost);
         }
+        float totalCost = cost + shipCost;
+
+        System.out.print("Your final total cost is £" + totalCost);
 
     }
+
+}
