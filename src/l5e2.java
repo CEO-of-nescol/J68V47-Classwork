ddf8a0cefbc1865cd4488b8957469109dd10fe1e
30184195 <30184195@nescol.ac.uk>
2023-09-27 15:06:36 +0100

    l5e2 and l5e3

---
+++ src/l5e2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class l5e2 {
+    public static void main(String[] args) {
+
+        for (int count = 10; count > 0; count--)
+        {
+            System.out.println(count);
+        }
+        System.out.println("Blastoff!!!!");
+    }
+}
