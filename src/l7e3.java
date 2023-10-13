62e16cd7b1d301cda7a759cfb7df77dc6acd89e5
30184195 <30184195@nescol.ac.uk>
2023-10-13 11:54:42 +0100

    l7e3 doing

---
+++ src/l7e3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class l7e3 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int score = 0;
+
+        for (int count = 1; count <= 10; count += 1) {
+            System.out.println("What is 5 x " + count);
+            int answer = input.nextInt();
+            if (answer == (5 * count)) {
+                score = score + 1;
+                System.out.println("Correct");
+            } else {
+                System.out.println("Incorrect, the answer is " + 5 * count);
+            }
+        }
+
+        System.out.println("Your total score is " + score + "/10");
+
+
+    }
+
+}
