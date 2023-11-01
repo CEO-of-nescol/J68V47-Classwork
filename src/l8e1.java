ce21b3bef11bb26437ec6cb1e54c1b0bcaac946d
30184195 <30184195@nescol.ac.uk>
2023-11-01 15:50:47 +0000

    l5e2 and l5e3

---
+++ src/l8e1.java
@@ -0,0 +1,8 @@
+public class l8e1 {
+    public static void main(String[] args) {
+        String[] fnames = {"Ben", "Alex", "Nathan", "Ash", "Charlie"};
+        System.out.print(fnames[0] + fnames[4] + fnames.length);
+    }
+
+}
+
---
+++ src/l8e2.java
@@ -0,0 +1,19 @@
+public class l8e2 {
+
+    public static void main(String[] args) {
+
+        int[] score = {92, 23, 56, 76, 28, 28, 59, 23, 87, 73};
+
+        for (int index = 0; index < score.length; index++) {
+            System.out.println(score[index]);
+        }
+        Float totalSc
+        for (int index = 0; index < score.length; index++) {
+
+            totalScore = totalScore + score[index];
+        }
+
+
+    }
+
+}
