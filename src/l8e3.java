722f54b9996d009f93ad08f507cfa35d17169ff5
30184195 <30184195@nescol.ac.uk>
2023-11-14 20:50:02 +0000

    FUNctions

--- src/l8e3.java
+++ src/l8e3.java
@@ -1,22 +1,28 @@
 public class l8e3 {
 
+
     public static void main(String[] args) {
         // Set up the directions array
+
+        newArray();
+
+        // Print out each direction
+
+    }
+
+    public static String[] newArray() {
         String[] directions = {"Start by exiting the building you are currently in and turning left onto the main road.",
                 "Walk down the road for two blocks until you reach the traffic lights.",
                 "At the traffic lights, turn right onto Oak Street.",
-                "Follow Oak Street for one block then turn left onto Elm Street.",
+                "Follow Oak Street for one block then turn left onto Elm Street.", "After turning onto Elm Street, walk past the grocery store on your right.",
                 "Walk to the end of Elm Street. Your doctor's office will be the third building on the right side of the road.",
-                "Enter through the front door of the office. Let the receptionist know you have arrived for your appointment.",
-                "Take a seat in the waiting room. The doctor will call your name when it's time to be seen. You've arrived!"
+                "Enter through the front door of the office. Let the receptionist know you have arrived for your appointment."
         };
-
-        // Print out each direction
         int step = 1;
-        for(String direction : directions) {
-            System.out.format("%d. %s %n",step,direction);
-            step ++;
+        for (String direction : directions) {
+            System.out.format("%d. %s %n", step, direction);
+            step++;
         }
+        return directions;
     }
-
 }
